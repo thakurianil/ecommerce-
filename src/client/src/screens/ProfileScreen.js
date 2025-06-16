febc58c332222a31c34fe8ec2fc9a04319326aa3
Anil khand <anilkhandthakuri@gmail.com>
2025-06-16 23:38:53 +1000

    Working on it

--- src/client/src/screens/ProfileScreen.js
+++ src/client/src/screens/ProfileScreen.js
@@ -7,11 +7,11 @@ import Loader from '../components/Loader';
 import { getUserDetails, updateUserProfile } from '../actions/userActions';
 
 const ProfileScreen = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [message, setMessage] = useState(null);
+  // const [name, setName] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [confirmPassword, setConfirmPassword] = useState('');
+  // const [message, setMessage] = useState(null);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
